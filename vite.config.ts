6d8bb5286d2f4f280e7a75550cf1d4bd3b3950f2
Evgeniy Talko <etalko@gmail.com>
2022-03-21 13:38:43 +0300

    build: add and setup vites and storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -5,6 +8,11 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: 'src/shared/setupTests.ts',
+    },
     plugins: [react(), tsconfigPaths()],
     resolve: {
         alias: [{ find: '@', replacement: path.resolve(process.cwd()) }],
