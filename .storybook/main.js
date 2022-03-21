da01e84e7d5b8ea02e36e12911697fbba45af85b
Evgeniy Talko <etalko@gmail.com>
2022-03-21 17:35:33 +0300

    build(storybook): add addons (a11y, measure)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,15 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-actions"
-  ],
-  "framework": "@storybook/react"
+    stories: [
+        '../src/**/*.stories.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/addon-actions',
+        '@storybook/addon-measure',
+        '@storybook/addon-a11y',
+    ],
+    framework: '@storybook/react',
 }
